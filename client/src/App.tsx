81facd12d64e4231ba696e69fb3001c8388a5d2c
Trian-Guru <131803402+trianguru@users.noreply.github.com>
2026-03-01 13:28:54 -0500

    Add Catalogue route to the Router component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { Switch, Route } from "wouter";
+import Catalogue from "@/pages/Catalogue";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -10,21 +11,30 @@ import Cart from "@/pages/Cart";
 import Dashboard from "@/pages/Dashboard";
 import About from "@/pages/About";
 import WaitlistSuccess from "@/pages/WaitlistSuccess";
+import Catalogue from "@/pages/Catalogue";
+
+
+
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
+        <Route path="/catalogue" component={Catalogue} />
       <Route path="/products" component={ProductList} />
       <Route path="/subscriptions" component={ProductList} /> {/* Reuse list with filters */}
       <Route path="/cart" component={Cart} />
       <Route path="/dashboard" component={Dashboard} />
+        <Route path="/catalogue" component={Catalogue} />
       <Route path="/about" component={About} />
       <Route path="/waitlist/success" component={WaitlistSuccess} />
       <Route component={NotFound} />
     </Switch>
   );
 }
+
+
+
 
 function App() {
   return (
@@ -34,7 +44,3 @@ function App() {
         <Router />
       </TooltipProvider>
     </QueryClientProvider>
-  );
-}
-
-export default App;
