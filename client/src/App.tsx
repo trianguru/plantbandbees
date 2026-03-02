079f08fa7057d1534e2959edf11287b12d682ed8
Trian-Guru <131803402+trianguru@users.noreply.github.com>
2026-03-01 22:05:53 -0500

    Fix App.tsx: remove duplicate imports and routes, clean up Router structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import { Switch, Route } from "wouter";
-import Catalogue from "@/pages/Catalogue";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -10,31 +9,22 @@ import ProductList from "@/pages/ProductList";
 import Cart from "@/pages/Cart";
 import Dashboard from "@/pages/Dashboard";
 import About from "@/pages/About";
-import WaitlistSuccess from "@/pages/WaitlistSuccess";
 import Catalogue from "@/pages/Catalogue";
-
-
-
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
-        <Route path="/catalogue" component={Catalogue} />
+      <Route path="/catalogue" component={Catalogue} />
       <Route path="/products" component={ProductList} />
-      <Route path="/subscriptions" component={ProductList} /> {/* Reuse list with filters */}
+      <Route path="/subscriptions" component={ProductList} />
       <Route path="/cart" component={Cart} />
       <Route path="/dashboard" component={Dashboard} />
-        <Route path="/catalogue" component={Catalogue} />
       <Route path="/about" component={About} />
-      <Route path="/waitlist/success" component={WaitlistSuccess} />
       <Route component={NotFound} />
     </Switch>
   );
 }
-
-
-
 
 function App() {
   return (
@@ -44,3 +34,7 @@ function App() {
         <Router />
       </TooltipProvider>
     </QueryClientProvider>
+  );
+}
+
+export default App;
